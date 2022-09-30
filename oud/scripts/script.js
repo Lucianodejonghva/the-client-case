1820faa7279e183de0a46e8c98160206266c044b
Lucianodejonghva <luciano.de.jong@hva.nl>
2022-09-30 13:34:56 +0200

    commit

---
+++ oud/scripts/script.js
@@ -0,0 +1,14 @@
+var map = L.map("map", {
+    center: [52.3376, 4.902],
+    zoom: 11,
+    zoomControl: false,
+  });
+  
+  L.tileLayer(
+    "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+    {
+      maxZoom: 20,
+      minZoom: 8,
+      attribution: "© Stadia Maps",
+    }
+  ).addTo(map);
